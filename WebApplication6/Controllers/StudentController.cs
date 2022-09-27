db2ae299de387f6ecddbaffeac0af8577c7d07ae
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-09-27 15:06:36 +0530

    first commit

---
+++ WebApplication6/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Details()
+        {
+            Student stu = new Student()
+            {
+                StudentId = 102,
+                Name = "mansi",
+                Branch = "CSE",
+                Gender ="f"
+            };
+            Address add = new Address()
+            {
+                StudentId = 102,
+                City = "delhi",
+                State = "Delhi",
+                Country = "India",
+                Pincode = "876569"
+            };
+            StudentDetailsViewModel studentDetailsViewModel = new StudentDetailsViewModel()
+            {
+                student = stu,
+                address = add,
+                Title = "Student Details",
+                Header = "Student Data"
+            };
+            return View(studentDetailsViewModel);
+        }
+    }
+}
---
+++ WebApplication6/Models/StudentDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public class StudentDetailsViewModel
+    {
+        public Student student { get; set; }
+        public Address address { get; set; }
+        public string Title { get; set; }
+        public string Header { get; set; }
+    }
+}
